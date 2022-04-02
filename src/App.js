5f955db49b3fa1f14dd8afc90512ae792e9beecf
Brayian <70912533+bramireza@users.noreply.github.com>
2022-04-01 23:34:17 -0500

    pruebas de props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Componente from './components/Componente';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <label htmlFor='nombre'>Nombre</label>
-        <input type="text" id='nombre'></input>
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </section>
+        <section>
+          <Componente msg="hola soy brayian Ramirez"></Componente>
+        </section> 
       </header>
+      
     </div>
   );
 }
