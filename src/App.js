7311eee32678d91f6a411a85238d1862ddd107c7
Brayian <70912533+bramireza@users.noreply.github.com>
2022-04-01 22:47:12 -0500

    Actualizacion prueba 1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <label htmlFor='nombre'>Nombre</label>
+        <input type="text" id='nombre'></input>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
